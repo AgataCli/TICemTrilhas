119a550c2284cf621c0c913da46baa09b190d24d
agataCSB <agata.brazao@icomp.ufam.edu.br>
2025-03-05 17:38:26 -0400

    adicionado exemplo de codigo de testagem

---
+++ fundamentos_testes_software/exemplo_modulo2_teste.py
@@ -0,0 +1,29 @@
+# Reprodução do código mostrado durante a trilha para experenciar sua execução
+# Código de Carlos Santana, professor da trilha
+
+import unittest
+
+def login(usuario,senha):
+	if usuario == "teste" and senha == "teste123":
+		return True
+
+	return False
+
+class TestLogin(unittest.TestCase):
+
+	def test_login_sucess(self):
+		usuario = "teste"
+		senha = "teste123"
+
+		resultado_login = login(usuario,senha)
+		self.assertTrue(resultado_login)
+
+	def test_login_error_wrong_pass(self):
+		usuario = "teste"
+		senha = "123"
+
+		resultado_login = login(usuario,senha)
+		self.assertFalse(resultado_login) #retorna verdadeiro, pois o resultado esperado é falso
+
+if __name__ == '__main__':
+	unittest.main()
